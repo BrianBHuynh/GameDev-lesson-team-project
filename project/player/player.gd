90599c9546a3a95a6211b2bf5ba4bdfef601e94b
chaserusso <chaserusso@users.noreply.github.com>
2024-12-05 14:08:43 -0500

    Map Objects
    dingles and berries

--- project/player/player.gd
+++ project/player/player.gd
@@ -113,3 +113,11 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	inRange.erase(body)
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	pass # Replace with function body.
