31d69723a60a095e7bf84f8fef3d1ad49be180b8
LoganJProcon <loganjprocon@gmail.com>
2024-12-05 14:04:01 -0500

    Fixed previous issues && made fixes to walking animations

--- project/player/player.gd
+++ project/player/player.gd
@@ -5,7 +5,7 @@ extends CharacterBody2D
 var health = 10
 var maxHealth = 10
 var SPEED = 100.0
-var stamina = 100
+var stamina = 100.0
 var stamRest = false
 const JUMP_VELOCITY = -400.0
 var save_dictionary : Dictionary
@@ -22,29 +22,31 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("Up"):
 		latestKey = "Up"
-	if Input.is_action_just_pressed("Down"):
+	elif Input.is_action_just_pressed("Down"):
 		latestKey = "Down"
-	if Input.is_action_just_pressed("Left"):
+	elif Input.is_action_just_pressed("Left"):
 		latestKey = "Left"
-	if Input.is_action_just_pressed("Right"):
+	elif Input.is_action_just_pressed("Right"):
 		latestKey = "Right"
-	if Input.is_action_pressed("Sprint") && !stamRest: #no infinite sprint
+	#Start of the Sprint && Stamina stuff
+	if Input.is_action_pressed("Sprint") && !stamRest && stamina >= 0: #no infinite sprint
 		SPEED = 150.0 #1.5 times the speed of the plc
-		stamina = stamina - 4 #reduce stamina so plc can't sprint infinitely
+		stamina = stamina - 2 #reduce stamina so plc can't sprint infinitely
 	else: #handles release of SHIFT key
 		SPEED = 100.0 #bring speed back to walking pace for plc
-		stamina = stamina + 2 #increase by 2 so there is no infinite sprint capability,
-							  #and so plc has to rest somewhat before they can sprint again
+		if stamina <= 100: #don't go over 100
+			stamina = stamina + 0.5 #increase by 2 so there is no infinite sprint capability,
+							  	  #and so plc has to rest somewhat before they can sprint again
 	
-	if stamina < 20: #if plc sprints till no stamina left then force plc to only walk or stay still till
+	if stamina < 5: #if plc sprints till almost no stamina left then force plc to only walk or stay still till
 		stamRest = true #stamina is full again
-	if stamina >= 400:
+	elif stamina >= 90:
 		stamRest = false #handles case of stamina being full enough again, then allows plc to sprint again
 	
-	
+	$StaminaBar.value = stamina
+	#End of the Sprint && Stamina stuff
 
 	$ProgressBar.value = health * 100 / maxHealth
-	$StaminaBar.value = stamina
 	
 	if health < 0:
 		queue_free()
@@ -58,35 +60,34 @@ func _physics_process(delta: float) -> void:
 	
 	if Input.is_action_pressed("Up"):
 		velocity.y = -SPEED
-	if Input.is_action_pressed("Down"):
+	elif Input.is_action_pressed("Down"):
 		velocity.y = velocity.y +SPEED
 	if Input.is_action_pressed("Left"):
 		velocity.x = -SPEED
-	if Input.is_action_pressed("Right"):
+	elif Input.is_action_pressed("Right"):
 		velocity.x = velocity.x +SPEED
 		
+	
 	if velocity.x == 0 and velocity.y == 0:
 		if latestKey == "Up":
 			$AnimatedSprite2D.play("idle_up")
-		if latestKey == "Down":
+		elif latestKey == "Down":
 			$AnimatedSprite2D.play("idle_down")
-		if latestKey == "Right":
+		elif latestKey == "Right":
 			$AnimatedSprite2D.play("idle_right")
-		if latestKey == "Left":
+		elif latestKey == "Left":
 			$AnimatedSprite2D.play("idle_left")
-	else:
-		if latestKey == "Up":
-			$AnimatedSprite2D.play("walk_up")
-		if latestKey == "Down":
-			$AnimatedSprite2D.play("walk_down")
-		if latestKey == "Right":
-			$AnimatedSprite2D.play("walk_right")
-		if latestKey == "Left":
-			$AnimatedSprite2D.play("walk_left")
 			
+	if velocity.x > 0: #Handles the case of plc pressing Up/Down
+		$AnimatedSprite2D.play("walk_right") #&& Right buttons at the same time and plays the walk_right animation
+	if velocity.x < 0: #Handles the case of plc pressing Up/Down
+		$AnimatedSprite2D.play("walk_left") #&& Left buttons at the same time and plays the walk_left animation
+	if velocity.x == 0 && velocity.y > 0: #Handles plc walking up
+		$AnimatedSprite2D.play("walk_down")
+	elif velocity.x == 0 && velocity.y < 0: #Handles plc walking down
+		$AnimatedSprite2D.play("walk_up")
 		
-		
-	
+	velocity = velocity.normalized()*SPEED
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 
