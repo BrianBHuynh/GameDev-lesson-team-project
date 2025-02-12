3061c48be730a3796216a5c4768bf76db2f4aa4b
dvasil23 <121838951+dvasil23@users.noreply.github.com>
2025-02-12 16:01:01 -0500

    guns and bullets
    added guns and bullets

---
+++ project/guns/shotgun.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	look_at(get_global_mouse_position())
+	if Input.is_action_just_pressed("attack"):
+		shoot()
+
+func shoot():
+	# Create a new instance of the Mob scene.
+	var mob: Node2D = load("res://project/bullets/bullet.tscn").instantiate()
+	get_tree().root.add_child(mob)
+	mob.global_position = $muzzle.global_position
